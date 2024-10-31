f48694e60a7da763cde49540a25f01728e8029da
ecasado <enrique.casado@fppiramide.com>
2024-10-31 12:24:44 +0100

    Enunciado Ejercicio6

---
+++ Unidad3/src/Ejercicio6.java
@@ -0,0 +1,10 @@
+/*
+Ejercicio6: Realiza un "mapa" con los nombres de los picos del Pirineo
+y su altitud. Comprueba que ocurre si intentas añadir el mismo pico más
+de una vez, tanto con la misma altura asignada como distinta.
+ */
+
+public class Ejercicio6 {
+
+    
+}
